1cd31de4965243d5f9668c5ac51b5f0966b3abb8
Cássio Antoniazzi <cassiosvaldo@gmail.com>
2016-08-01 14:58:23 -0300

    file that Checks distance and post tweet.

---
+++ proximity_tweet.js
@@ -0,0 +1,21 @@
+var five = require("johnny-five");
+var Twit = require('./twitter_bot.js');
+
+var board = new five.Board();
+var lastPostDate = null;
+
+board.on("ready", function() {
+    var proximity = new five.Proximity({
+        controller: "HCSR04",
+        pin: 7
+    });
+
+    proximity.on("data", function() {
+        var now = new Date();
+        if (Twit.canIpost(now.getTime()) && (this.cm < 10)) {
+            Twit.postTweet(this.cm);
+            console.log("posted - distance " + this.cm);
+        }
+    });
+});
+
